3d0416d9cb2312c7d7920139985de2d12ed8cb1f
Travis Worker (from Travis CI) <twstartups.service@gmail.com>
2020-08-21 04:33:46 +0000

    Deploy TWstartups/twstartups.github.io to github.com/TWstartups/twstartups.github.io.git:master

---
+++ precache-manifest.f09a564cf3d40252b4c0cc77b4c75624.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1518908ddaed97e81c3744b4f47018",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9620548c24274490e08",
+    "url": "./static/css/main.27b74f14.chunk.css"
+  },
+  {
+    "revision": "f2bed79b0958ad018e10",
+    "url": "./static/js/2.066fd6f6.chunk.js"
+  },
+  {
+    "revision": "360978b69b2d355b7f6ed150ede4e119",
+    "url": "./static/js/2.066fd6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9620548c24274490e08",
+    "url": "./static/js/main.56d72b53.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "48acc2bbbb80fc4573c6fb4553a1356e",
+    "url": "./static/media/sign-in-image.48acc2bb.svg"
+  },
+  {
+    "revision": "128e0a615bce8b0e7f7c88e900c9a4b0",
+    "url": "./static/media/sign-up-image.128e0a61.svg"
+  }
+]);
